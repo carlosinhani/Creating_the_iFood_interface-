6a0d61454a4e7cad413178c17a3f8ea55f57c999
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-23 19:20:13 -0300

    iFood_finished

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./pages/Home";
-import Main from "./pages/Main";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import Home from './Pages/Home';
+import Main from './Pages/Main';
 
 const Stack = createStackNavigator();
+
 const screenOptions = {
     headerShown: false
 }
-const Routes = ()=>{
-    return(
+
+const Routes = () => {
+
+    return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Main" screenOptions={screenOptions}>
+            <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Main" component={Main} />
             </Stack.Navigator>
