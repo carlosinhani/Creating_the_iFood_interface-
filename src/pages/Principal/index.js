2d89c218d0b8a08cddbba52fa74e2230f3864a41
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-23 15:06:34 -0300

    add_useState_useEffect

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Text, Alert, ActivityIndicator} from 'react-native';
 import { SafeAreaView, Image, View, ButtonContainer } from "./styles";
-import { Text } from "react-native";
-import banner from '../../assets/img/banner.png';
-import Button from '../../components/Button';
+
+// import banner from '../../assets/img/banner.png';
+// import Button from '../../components/Button';
 
 export default function Principal() {
+
+  const [banner, setBanner] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  const [restaurantes, setRestaurantes] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+   useEffect(() => {
+    async function buscaDados(){
+       try{
+
+           const reponse = await fetch('http://my-json-server.typicode.com/pablohdev/app-ifood-clone/db');
+           const data = await Response.json();
+
+           setLoaded(true);
+
+           setBanner(data.banner_principal);
+           setCategorias(data.categorias);
+           setRestaurantes(data.restaurantes);
+           
+       }catch(e){
+         Alert.alert('Erro ao consultar'+e)
+       }
+    }
+
+    buscaDados();
+  },[])
+
+
+
   return (
     <>
        <StatusBar style="theme-dark" />
