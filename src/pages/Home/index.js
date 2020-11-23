4d4d2b50dd14a52ad880aaaee4b31b14bc584f4b
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-23 11:18:27 -0300

    add_button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
-import { SafeAreaView } from "./styles";
+import { SafeAreaView, Image, View, ButtonContainer } from "./styles";
+import { Text } from "react-native";
+import banner from '../../assets/img/banner.png';
+import Button from '../../components/Button';
 
 export default function Home() {
   return (
     <>
        <StatusBar style="theme-dark" />
        <SafeAreaView>
+         <View>
+           <Text>Pedir comida nunca foi tão facil</Text>
+           <Image source={banner} />
+           <Text>Permitir localização</Text>
+           <Text>Para descobrir restaurantes que entregam em sua região</Text>
+         </View>
+         <ButtonContainer>
+              <Button text="PULAR" onPress={() => {}} />
+              <Button text="ENTRAR" theme="primary" />
+         </ButtonContainer>
        </SafeAreaView>   
     </>      
   );
