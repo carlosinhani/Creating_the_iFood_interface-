07c3d92388974513f48eef3685d3ae5f11327000
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-23 10:27:39 -0300

    style_component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from "./styles";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>huhuhu</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+       <StatusBar style="theme-dark" />
+       <SafeAreaView>
+       </SafeAreaView>   
+    </>      
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import Home from "./pages/Home";
 
 
 const Stack = createStackNavigator();
+const screenOptions = {
+    headerShown: false
+}
 const Routes = ()=>{
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={screenOptions}>
                 <Stack.Screen name="Home" component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
